d2537aec1826771f049fa536c6ba97384933af58
SiddharthAdak <siddharthadak18@gmail.com>
2024-02-12 10:37:16 +0530

    added https api

--- src/api/persons.tsx
+++ src/api/persons.tsx
@@ -1,5 +1,5 @@
 export const fetchPersons = async (page:number, search: string) => {
-    const res = await fetch(`http://swapi.dev/api/people/?page=${page}&search=${search}`);
+    const res = await fetch(`https://swapi.dev/api/people/?page=${page}&search=${search}`);
     return res.json();
   }
   
